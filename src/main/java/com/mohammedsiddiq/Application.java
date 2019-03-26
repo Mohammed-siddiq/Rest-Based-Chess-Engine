deff06732d65c2801e8a79088e97e2d5f73e766b
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-03-25 22:15:52 -0500

    Added support for auto play with REST based opponent

--- src/main/java/com/mohammedsiddiq/Application.java
+++ src/main/java/com/mohammedsiddiq/Application.java
@@ -1,20 +1,11 @@
-package com.mohammedsiddiq.ChessGame;
+package com.mohammedsiddiq;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.mohammedsiddiq.ChessGame.DTOs.Move;
-import com.mohammedsiddiq.ChessGame.DTOs.StartGameResponse;
-import com.mohammedsiddiq.ChessGame.RestClient.GameClient;
-import com.mohammedsiddiq.ChessGame.Service.GameService;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
+import com.mohammedsiddiq.Configs.Configuration;
+import com.mohammedsiddiq.Service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import retrofit2.Call;
-import retrofit2.Response;
-import retrofit2.Retrofit;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -30,8 +21,11 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        //based on the configuration run as client player (who initiates first request for game) or server(who responds to the initial request
-        //Loading Configs
+
+        // If auto play is configured then start playing with the opponent.
+        if (Boolean.parseBoolean(args[1]))
+            service.autoPlay();
+
 
     }
 }
